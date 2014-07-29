c46b5d5ba6217b6483897eba36ae051ccbb5e5b4
Shehzan Devani <shehzandevani@gmail.com>
2014-07-29 15:42:34 -0700

    Create empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crm::Application.config.secret_key_base = '6810b88011d377bda17c86d218e067df9027117e6cd75bdffc68aabdf5788e6df84bc1f6651e12191d428054cd71f252e15a67766934fb7f0c43685abbb29191'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crm::Application.config.session_store :cookie_store, key: '_crm_session'

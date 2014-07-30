3ee4f31dc68d4b3b7eb2027943004958b06cc185
DJ Daniels <dj.daniels90@gmail.com>
2014-07-30 16:53:31 -0700

    created base transaction script class and implemented ostruct, created speck helper file and shared spec for transaction script inherited classes to use.
    added some development gems
    
    created the base tscript class and created specs for the base class
    
    completed tscripts base
    
    added comments to tscript class, implemented ostruct
    
    changes to the transaction script specs
    
    passes all tests, writing notes now
    
    refactored transactionscript spec to be modular and to be used in all other transactio script specs
    
    passes all tests, writing notes now
    
    refactored transactionscript spec to be modular and to be used in all other transactio script specs
    
    Update spec_helper.rb

---
+++ spec/services/transactionscript_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+#please describe the transaction script class in the same way as below in order to
+#maintain the form of the tests
+
+#this refers to the transactionscript_spec_helper and needs to be used at the
+#beginning of every transaction script.
+
+shared_examples_for "TransactionScripts" do
+  let(:current_script) { described_class.new }
+
+  it "exists" do
+    expect(described_class).to be_a(Class)
+    expect(described_class.new).to be_a(TransactionScript)
+  end
+
+  it "can fail" do
+    expect(current_script).to respond_to(:failure)
+    result = current_script.failure("error_code")
+    expect(result.success?).to eql(false)
+    expect(result.error).to eql("error_code")
+  end
+
+  it "can fail with data" do
+    result = current_script.failure("error_code", data: "someData")
+    expect(result.success?).to eql(false)
+    expect(result.error).to eql("error_code")
+    expect(result.data).to eql("someData")
+  end
+
+  it "can succeed" do
+    expect(current_script).to respond_to(:success)
+    result = current_script.success(data: "somedata")
+    expect(result.success?).to eql(true)
+    expect(result.data).to eql("somedata")
+  end
+
+end
+
+describe TransactionScript do
+
+#this refers to the transactionscript_spec_helper and needs to be used at the
+#beginning of every transaction script.
+  it_behaves_like('TransactionScripts')
+end
+

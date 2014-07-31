83a472e7946ec389e7873a45988335eb81ae8450
clnoll <noll.catherine@gmail.com>
2014-07-31 09:47:40 -0700

    fix migration error

--- db/migrate/20140731011907_update_box_history.rb
+++ db/migrate/20140731011907_update_box_history.rb
@@ -1,7 +1,5 @@
 class UpdateBoxHistory < ActiveRecord::Migration
   def change
-    change_table :box_history do |t|
-      add_column :stage_from
-    end
+    add_column :box_history, :stage_from, :integer
   end
 end

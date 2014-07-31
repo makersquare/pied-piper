1c688909b95fdadc11f4990b809b5c351d880c32
DJ Daniels <djdaniels90@users.noreply.github.com>
2014-07-31 14:03:32 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-
-
-
 RSpec.configure do |config|
   # ## Mock Framework
   #

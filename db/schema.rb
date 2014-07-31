402963c5724825947b27095f1aa01093b4a2ad09
clnoll <noll.catherine@gmail.com>
2014-07-31 09:47:40 -0700

    fix migration error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730234921) do
+ActiveRecord::Schema.define(version: 20140731011907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140730234921) do
     t.integer  "stage_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stage_from"
   end
 
   create_table "boxes", force: true do |t|

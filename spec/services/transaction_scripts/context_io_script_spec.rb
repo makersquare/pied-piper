83be74e0c35dc3a12b7d72c6d1e65612d9b6058b
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-01 16:24:35 -0700

    create context_io transaction script and spec file

---
+++ spec/services/transaction_scripts/context_io_script_spec.rb
@@ -0,0 +1,28 @@
+inlcude 'spec_helper'
+  it_behaves_like('TransactionScripts')
+
+description 'context_io' do
+
+  context 'recieves a notification from context_io webhook' do
+    before(all:)
+    it 'authenticates that the request came from context.io' do
+
+    end
+
+    it 'parses the notification into a hash' do
+    end
+
+    it 'confirms that it is the correct type of notification for typeform' do
+    end
+
+    it 'parses the email body into a hash' do
+    end
+
+  end
+
+
+  it 'sends the info to the api transaction scripts' do
+
+  end
+
+end

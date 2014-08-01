996d25f3c82ef1e0b43d82b69e407deada2b989d
clnoll <noll.catherine@gmail.com>
2014-08-01 16:40:14 -0700

    get_contacts_by_pipeline_id txs finished

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,6 +1,6 @@
 class Box < ActiveRecord::Base
-  belongs_to :pipelines
-  belongs_to :contacts
+  belongs_to :pipeline
+  belongs_to :contact
   has_many :fields, through: :box_fields
   has_many :box_histories
   has_many :notes
--- app/services/get_box_by_contact_and_pipeline_ids.rb
+++ app/services/get_box_by_contact_and_pipeline_ids.rb
@@ -1,6 +1,6 @@
 class GetBoxByContactAndPipelineIds < TransactionScript
   def run(params)
-    # binding.pry
+    # Return a box associated with given contact and pipeline ids
     result = Box.first(:conditions=>["pipeline_id = ? AND contact_id = ?", params[:pipeline_id], params[:contact_id]])
     return success(box: result)
   end

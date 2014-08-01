cf23404a1cc2f1207b7fe7893c7ad3368344fed6
clnoll <noll.catherine@gmail.com>
2014-08-01 12:40:18 -0700

    add comments to add_contact_to_pipeline txs

--- app/services/add_contact_to_pipeline.rb
+++ app/services/add_contact_to_pipeline.rb
@@ -1,5 +1,6 @@
 class AddContactToPipeline < TransactionScript
   def run(params)
+    # Add contact to a pipeline via the ContactPipelines table
     result = ContactPipelines.create(params)
     return success(contact: result)
   end

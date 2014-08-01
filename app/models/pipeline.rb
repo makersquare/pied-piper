07238fd4eceb99063ddec34ff4e90e85ddf4c74b
DJ Daniels <dj.daniels90@gmail.com>
2014-08-01 10:37:08 -0700

    resolved merge conflicts for pre development merge rebase

--- app/models/pipeline.rb
+++ app/models/pipeline.rb
@@ -2,15 +2,5 @@ class Pipeline < ActiveRecord::Base
   has_many :stages
   has_many :pipeline_users
   has_many :users, :through => :pipeline_users
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 3134d99... Created Models and migrations, integrating with contacts
-=======
   validates :name, uniqueness: true
->>>>>>> 890a446... wrote tests and tsx for crreate pipeline, bug in
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   has_many :pipeline_users
   has_many :pipelines, :through => :pipeline_users
-<<<<<<< HEAD
+
 end
-=======
-end
->>>>>>> 3134d99... Created Models and migrations, integrating with contacts

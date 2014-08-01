a21b8df07416984e027a777248f5bb3e9a8484ef
vagrant <vagrant@student-dev-box>
2014-08-01 18:54:02 -0500

    finished transaction scripts for box and contact

--- app/services/add_contact_to_pipeline.rb
+++
@@ -1,7 +0,0 @@
-class AddContactToPipeline < TransactionScript
-  def run(params)
-    # Add contact to a pipeline via the ContactPipelines table
-    result = ContactPipeline.create(params)
-    return success(contact: result)
-  end
-end
--- app/services/get_box_by_contact_and_pipeline_ids.rb
+++
@@ -1,7 +0,0 @@
-class GetBoxByContactAndPipelineIds < TransactionScript
-  def run(params)
-    # Return a box associated with given contact and pipeline ids
-    result = Box.first(:conditions=>["pipeline_id = ? AND contact_id = ?", params[:pipeline_id], params[:contact_id]])
-    return success(box: result)
-  end
-end

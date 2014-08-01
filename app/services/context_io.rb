36c81cca1d6213b9af57c7ada6350ec9e171e89e
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-01 16:28:02 -0700

    cleaned up context.io tx script files

--- app/services/context_io.rb
+++
@@ -1,7 +0,0 @@
-require 'contextio'
-
-contextio = ContextIO.new('hmsdfr9u', 'AGDc0NnvjJicuwaA')
-
-account = contextio.accounts.where(email: 'devpiedpiper@gmail.com').first
-
-account.messages.where(from: 'jeredmccullough@gmail.com')
--- app/services/context_io_script.rb
+++ app/services/context_io_script.rb
@@ -2,6 +2,16 @@ require 'json'
 require 'openssl'
 require 'Base64'
 
+
+# require 'contextio'
+
+# contextio = ContextIO.new('hmsdfr9u', 'AGDc0NnvjJicuwaA')
+
+# account = contextio.accounts.where(email: 'devpiedpiper@gmail.com').first
+
+# account.messages.where(from: 'jeredmccullough@gmail.com')
+
+
 class ContextIOTrigger < TransactionScript
   def intialize(webhook_id, account_id, api_key, secret_key)
     @webhook_id = webhook_id

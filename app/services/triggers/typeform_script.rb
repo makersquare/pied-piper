5db6bb360e1d2f65eadae4c0b20357aae630ba28
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-01 17:27:20 -0700

    fix incomplete files

---
+++ app/services/triggers/typeform_script.rb
@@ -0,0 +1,20 @@
+class TypeFormTrigger
+  def run
+    api_call
+  end
+
+####  will return JSON ####
+  def api_call
+  end
+end
+
+
+class TypeFormTrigger < TransactionScript
+  def run(inputs)
+
+  end
+end
+
+
+# 1. go through the api call and make sure it has the information we need
+# 2.

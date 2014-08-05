0a8498aeb11d5ba2923092e6cba4dc6821d6ff6e
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-04 18:58:05 -0700

    Remove uid from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805014308) do
+ActiveRecord::Schema.define(version: 20140805015631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 20140805014308) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
-    t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
   end

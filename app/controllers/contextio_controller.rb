68e657e3ba20f754c0e4ffcba591a0e0e569d395
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-05 12:59:17 -0700

    implemented api transaction scripts in the contextio controller

--- app/controllers/contextio_controller.rb
+++ app/controllers/contextio_controller.rb
@@ -2,6 +2,18 @@ class ContextioController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:callback]
 
   def callback
-    puts params
+    contact = ApiConversionScript.run(params)
+    puts contact unless contact.error.nil?
+
+    contact = ApiAuthenticationScript.run(params)
+    puts contact unless contact.error.nil?
+
+    contact = RetrieveMessageScript.run(params)
+    puts contact unless contact.error.nil?
+
+    contact = ParsingEmailScript.run(params)
+    puts contact unless contact.error.nil?
+
+    puts contact
   end
 end

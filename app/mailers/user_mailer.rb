a8221cdcf3bfe1be8f372056a6c8ed2e2d96a756
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-04 17:52:45 -0700

    Implement ActionMailer view, user_mailer.rb, and smtp settings

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,26 @@
+class UserMailer < ActionMailer::Base
+  # default from: "dev.piedpiper@gmail.com"
+  default from: "test@example.com"
+
+  # def welcome_email(user)
+  #   @user = user
+  #   @url = 'http://makersquare.com/crm/admin'
+  #   mail(to: @user.email, subject: 'Welcome to Pied Piper CRM')
+  # end
+
+  def pipeline_update
+    mail(to: 'gabe.a.polk@gmail.com', subject: 'This is a test')
+  end
+
+  # def pipeline_update
+  #   @user = User.all
+  #   @user.each do |user|
+  #     mail(to: user.email,
+  #          subject: 'Pipeline Update',
+  #          template_path: '',
+  #          template_name: 'another')
+  #   end
+  # end
+end
+
+# UserMailer.pipelineUpdate.deliver

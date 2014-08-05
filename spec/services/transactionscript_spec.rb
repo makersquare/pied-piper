989d052fdc3a4537966b1074bf49277515228657
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-04 18:41:34 -0700

    rebasing

--- spec/services/transactionscript_spec.rb
+++ spec/services/transactionscript_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 #please describe the transaction script class in the same way as below in order to
 #maintain the form of the tests
 
+
 #this refers to the transactionscript_spec_helper and needs to be used at the
 #beginning of every transaction script.
 
-describe TransactionScript do
-#this refers to the transactionscript_spec_helper and needs to be used at the
-#beginning of every transaction script.
   it_behaves_like('TransactionScripts')
 end

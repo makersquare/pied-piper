e43374959afc5a0b9fbfa77a8a06aebf3b34a2ff
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-06 11:20:16 -0700

    added 2 methods to create contact TX script. contact_already_exists? to test for copies in the database and contact_has_name? confirm the perspective contact has a name

--- app/services/create_contact.rb
+++ app/services/create_contact.rb
@@ -1,6 +1,35 @@
+require 'ostruct'
+
 class CreateContact < TransactionScript
   def run(params)
-    result = Contact.create(params)
+    params = OpenStruct.new(params) if params.is_a?(Hash)
+    return failure 'Contact already in database' if contact_already_exists?(params)
+    return failure 'Contact is missing name' unless contact_has_name(params)
+    params = params.to_h
+
+    result = Contact.create({name: params.name, phoneNum: params.phoneNum, email: params.email})
     return success(contact: result)
   end
+
+  def contact_already_exists?(params)
+
+    email = Contact.find_by!(email: params.email).email
+    name = Contact.find_by!(name: params.name).name
+    name = Contact.find_by!(name:'#{params.firstname} #{params.lastname}') if name.nil?
+    phoneNum = Contact.find_by!(phoneNum: params.phoneNum).phoneNum
+    phoneNum = Contact.find_by!(phoneNum: params.phonenumber).phoneNum if phone.nil?
+
+    if !name.nil? && !phonNum.nil? && !email.nil?
+      return true
+    end
+
+  end
+
+  def contact_has_name?(params)
+    params.phoneNum = params.phonenumber if params.phonNum.nil?
+    params.name = name:'#{params.firstname} #{params.lastname}') if params.name.nil?
+    return false if name.nil?
+  end
+
+
 end

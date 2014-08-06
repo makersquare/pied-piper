54411bf011fa0529ed92ed20849f5819cd8453cf
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-06 12:41:45 -0700

    new tx script to access contacts from contextio and upload them into the database

--- app/services/download_contacts_script.rb
+++ app/services/download_contacts_script.rb
@@ -1,2 +1,32 @@
 class DownloadContactScript < TransactionScript
+  def run(inputs)
+    contextio = ContextIO.new('hmsdfr9u', inputs.secret_key)
+    account = contextio.accounts[inputs.alert['account_id']]
+    message = get_message(inputs, account)
+
+    return failure 'message content missing' unless message.is_a?(ContextIO::Message)
+
+    text = select_message_text(message)
+
+    return failure text['error'] unless text['error'].nil?
+    return failure 'message content not a string' unless text.is_a?(String)
+
+    return success ({ alert: text, secret_key: inputs.secret_key  })
+  end
+
+  def get_message(inputs, account)
+
+    message_id = inputs.alert['message_data']['message_id']
+    account.messages[message_id]
+
+  end
+
+  def select_message_text(message)
+    begin
+      message.body_parts.where(type: 'text/plain').first.content
+    rescue
+      'message not formatted correctly'
+    end
+  end
+
 end

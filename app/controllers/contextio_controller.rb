b0a04352c11c1891023a5950bdb3c0e0390cc32e
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-05 22:17:40 -0700

    add notes to the contextio scripts, specifically"

--- app/controllers/contextio_controller.rb
+++ app/controllers/contextio_controller.rb
@@ -1,6 +1,7 @@
 class ContextioController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:callback]
 
+#This controller is for the Contextio notification post
   def callback
     ContextioApiScript.run(params)
     render nothing: true

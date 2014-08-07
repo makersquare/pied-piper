a680fb4f5eea36627c669f879d4b624cc2b15e9e
andrewpei <andrewpei@gmail.com>
2014-08-06 17:36:08 -0700

    Made migration and updated AR to account for hiding stages per user per pipeline. Also deleted uid from users
    Delete uid from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805175311) do
+ActiveRecord::Schema.define(version: 20140807003732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,13 +80,18 @@ ActiveRecord::Schema.define(version: 20140805175311) do
     t.datetime "updated_at"
   end
 
+  create_table "user_stages", force: true do |t|
+    t.integer "user_id"
+    t.integer "stage_id"
+    t.boolean "hidden"
+  end
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
-    t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
   end

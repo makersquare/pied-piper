dbd9d2388e6eadedd09f3ddc6e0ac2d48dcfd90d
andrewpei <andrewpei@gmail.com>
2014-08-07 10:44:47 -0700

    Updated seeds to make box fields

--- app/services/retrieve_pipeline.rb
+++ app/services/retrieve_pipeline.rb
@@ -9,6 +9,26 @@ class RetrievePipeline < TransactionScript
     pipeline_stages.each { |stage|
       stage_boxes[stage.id] = stage.boxes
     }
+
     return success(:data => {pipeline: pipeline_entity, pipeline_stages: pipeline_stages, stage_boxes: stage_boxes})
   end
 end
+
+
+# {
+#   pipeline: {
+#     entity: ___,
+#     fields: ["attribute1", "attribute2"],
+#     stages: [
+#       {
+#         entity: ___,
+#         boxes: [
+#           {
+#             contact_entity: ___,
+#             fields: [{attribute1: "value1", attribute2: "value2"}]
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# }

6c6be8cbcd6e496fcad7f65eda91c6711bb4c491
andrewpei <andrewpei@gmail.com>
2014-08-07 15:32:35 -0700

    Created model and migration for stages and fields, no CRUD

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,4 +2,6 @@ class Field < ActiveRecord::Base
   has_many :box_fields
   has_many :boxes, through: :box_fields
   belongs_to :pipeline
+  has_many :stage_fields
+  has_many :stages, through: :stage_fields
 end

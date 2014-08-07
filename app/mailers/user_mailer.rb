00e45e9814c122a046f96eca1882708a73270200
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-07 13:51:38 -0700

    Remove unnecessary comments + code from user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,2 @@
 class UserMailer < ActionMailer::Base
-  # def welcome_email(user)
-  #   @user = user
-  #   @url = 'http://makersquare.com/crm/admin'
-  #   mail(to: @user.email, subject: 'Welcome to Pied Piper CRM')
-  # end
-
-  def pipeline_update
-    mail(to: 'gabe.a.polk@gmail.com', subject: 'This is a test')
-  end
-
-  # def pipeline_update
-  #   @user = User.all
-  #   @user.each do |user|
-  #     mail(to: user.email,
-  #          subject: 'Pipeline Update',
-  #          template_path: '',
-  #          template_name: 'another')
-  #   end
-  # end
 end
-
-# UserMailer.pipelineUpdate.deliver

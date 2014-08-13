68d3469be1fceabcf6968b4b9733859a9f33fb16
Shehzan Devani <shehzandevani@gmail.com>
2014-08-13 16:00:15 -0700

    First attempts. Does not work.

--- app/assets/javascripts/angular/controllers/pipeline_details_ngcontroller.js
+++ app/assets/javascripts/angular/controllers/pipeline_details_ngcontroller.js
@@ -1,24 +1,3 @@
-app.directive('dndList', function(){
-
-// Custom directive for drag/drop between stages
-    // Watch for changes to contact; use "true" to receive updates when values change
-    return function(scope, element, attrs){
-      console.log(scope)
-      // $(contact).sortable({
-      //   items: 'form',
-      //   start:function(event, ui){
-
-      //   },
-      //   stop:function(event, ui){
-      //     contact.stage = 1
-      //   },
-      //   axis: 'y'
-      // })
-
-      // ContactBoxRsc.update(contact);
-    }
-})
-
 app.controller('PipelineDetailsCtrl',
   ['$scope', '$resource',
   '$routeParams','PipelinesRsc',
@@ -40,8 +19,19 @@ app.controller('PipelineDetailsCtrl',
     $scope.fields = FieldsRsc.query({pipeline_id: $routeParams.id});
 
     $scope.contacts = ContactsBoxRsc.query({pipeline_id: $routeParams.id});
-
     $scope.stages = StagesRsc.query({pipeline_id: $routeParams.id});
+
+    $scope.contacts.$promise.then(function(data){
+      $scope.contacts = data;
+      return $scope.stages.$promise;
+    }).then(function(data){
+      $scope.stages = data;
+      mapContactsToStages($scope.contacts, $scope.stages);
+    });
+
+    var mapContactsToStages = function(contacts, stages) {
+      
+    }
 
 // Edit the entry in the browser by double-clicking the text; press enter to update in the database
     $scope.keyup = function(event, contact) {
@@ -53,18 +43,17 @@ app.controller('PipelineDetailsCtrl',
         ContactBoxRsc.update(contact);
       }
     };
-    $('.contacts').sortable();
 
-    $scope.$watch(dndList, function(value){
-      console.log("Contact: " + value)
-    })
-    // // Watch for changes to contact; use "true" to receive updates when values change
-    // $scope.$watch($scope.contact, function(value){
-    //   console.log("Contact: " + value//.map(function(e){
-    //     //return e.id //????
-    //   // }
-    //   )//.join(','));
-    // }, true);
+    $scope.changeStage = function(contact, stage){
+      console.log(contact)
+      console.log(stage)
+      // var index = $scope.contacts.indexOf(contact);
+      // $scope.contacts.splice(index, 1);
+      contact.stage_id = stage.id;
+      // ContactBoxRsc.update(contact).$promise.then(function(contact) {
+      //   $scope.contacts.push(contact);
+      // });
+    }
 
 // Define the rails path that will be hit by the http requests
     var ContactBoxRsc = $resource('/pipelines/:pid/contacts/:cid.json',

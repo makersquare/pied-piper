6a440292302197349daa736f92dfac0cb81d4f3b
andrewpei <andrewpei@gmail.com>
2014-08-14 15:27:21 -0700

    Fixed spacing

--- app/services/ContextIO/retrieve_all_email.rb
+++ app/services/ContextIO/retrieve_all_email.rb
@@ -4,12 +4,12 @@ class RetrieveAllEmail < TransactionScript
   def run(contact)
     messages =[]
     Users.all.each do |user|
-      user_messages = get_messages({ user:user, contact:contact, time: 4.weeks.ago.to_i }})
+      user_messages = get_messages({ user:user, contact:contact, time: 4.weeks.ago.to_i })
       user_messages.each do |message|
         messages << { user_email: user.email, subject: message.subject, date: Time.at(message.date), message_id:message.message_id}
       end
     end
-  success { contact: contact, messages: messages }
+  success(contact: contact, messages: messages )
   end
 
 

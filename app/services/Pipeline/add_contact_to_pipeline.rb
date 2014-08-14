fbc901d92322dc72ab149d8ac3928574ee36d713
Gabe Polk <gabe.a.polk@gmail.com>
2014-08-14 10:26:56 -0700

    Remove comments from AddContactToPipeline txs

--- app/services/Pipeline/add_contact_to_pipeline.rb
+++ app/services/Pipeline/add_contact_to_pipeline.rb
@@ -2,7 +2,6 @@ class AddContactToPipeline < TransactionScript
   def run(params)
     # Add contact to a pipeline via the ContactPipelines table
     result = Box.create(params)
-    # if stage id == null then populate with first stage # pipeline.stages.first
     return success(contact: result)
   end
 end

7df38c9a27e3174ec6f5a250b566761737dd7378
William Harris <wharris@upscalews.com>
2014-08-14 17:29:23 -0700

    one passing and one failing test
    e2e test running instuction for vagrant vms:

--- spec/javascripts/e2e_specs/example_spec.js
+++ spec/javascripts/e2e_specs/example_spec.js
@@ -1,5 +1,14 @@
 describe('Example Specifcation', function() {
+
+    beforeEach(function() {
+        browser.get('/#/pipelines');
+    });
+
     it('should be able to find the rails test server', function() {
         expect(true).toBeTruthy();
+    })
+
+    it('title should be correct', function() {
+      expect(browser.getTitle()).toEqual('Super Calculator');
     })
 });

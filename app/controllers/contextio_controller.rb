3baa8a4345af944bab46fe86693abd7e712580ee
Jered McCullough <mathias.armstrong@gmail.com>
2014-08-16 02:14:09 -0700

    testing for webhook access

--- app/controllers/contextio_controller.rb
+++ app/controllers/contextio_controller.rb
@@ -2,6 +2,7 @@ class ContextioController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:callback]
 #This controller is for the Contextio notification post
   def callback
+    puts params
     ContextioApiScript.run(params)
     render nothing: true
   end

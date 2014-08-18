9affe410146037145a9ce426b7f7022753fc76f1
alex haines <hainesaxiv@gmail.com>
2014-08-18 14:53:29 -0700

    refacorted stages to include an id

--- app/assets/javascripts/angular/controllers/stages_ngcontroller.js
+++ app/assets/javascripts/angular/controllers/stages_ngcontroller.js
@@ -1,39 +1,31 @@
-//This factory establishes a restful http api that talks to the stages controller in rails
-app.factory('StagesAPI', function($resource) {
-  return $resource('/pipelines/:pipeline_id/stages/:id.json',
-    {pipeline_id: '@pipeline_id', stage_id: '@id'}, {
-    update: {
-          method: 'PUT'
-        }
-    });
-});
-
 //this custom directive gives functionality to delete buttons on the dom to
 //delete stages in the dom and database simultaneously
-app.directive('deletestage', function(StagesAPI, $routeParams) {
+app.directive('deletestage',
+  ['StagesRsc', '$routeParams',
+  function(StagesRsc, $routeParams) {
   return function(scope, element) {
     element.bind('click', function(e){
       e.preventDefault();
-      StagesAPI.remove({pipeline_id: $routeParams['pipeline_id'], id: this.dataset.id}, element, function(element){
+      StagesRsc.remove({pipeline_id: $routeParams['pipeline_id'], id: this.dataset.id}, element, function(element){
         scope.stages.splice(element.context.dataset.index, 1);
       });
     });
   };
-});
+}]);
 
 //this controller gives scope to the dom template "createFields.html" and adds
 //functionality for adding stages to the dom and db simultaneously
 app.controller('StagesCtrl',
-  ['$routeParams', '$scope', '$http', 'StagesAPI',
-  function($routeParams, $scope, $http, StagesAPI) {
+  ['$routeParams', '$scope', '$http', 'StagesRsc',
+  function($routeParams, $scope, $http, StagesRsc) {
 
-  $scope.stages = StagesAPI.query({pipeline_id: $routeParams['pipeline_id']});
+  $scope.stages = StagesRsc.query({pipeline_id: $routeParams['pipeline_id']});
 
   $scope.addStage = function() {
-    $scope.stage = new StagesAPI();
+    $scope.stage = new StagesRsc();
     $scope.stage.$save({pipeline_id: $routeParams['pipeline_id'], name: $scope.stageName, description: $scope.stageDesc, pipeline_location: $scope.stageLoc},
       function(data) {
-        $scope.stages.push({name: $scope.stageName, description: $scope.stageDesc, pipeline_location: $scope.stageLoc});
+        $scope.stages.push({name: $scope.stageName, description: $scope.stageDesc, pipeline_location: $scope.stageLoc, id: data.id});
         $scope.stageName = "";
     });
   };

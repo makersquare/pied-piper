5fc027718612bf704a2b0a9e0aa0aa2e3b85c57b
DJ Daniels <dj.daniels90@gmail.com>
2014-08-18 13:31:24 -0700

    changed hash index to hash fetch within the validate request script for greater verbosity

--- app/services/authenticate_request_script.rb
+++ app/services/authenticate_request_script.rb
@@ -1,11 +1,13 @@
 
 class AuthenticateRequestScript < TransactionScript
   def run(params)
-    # check that the token returns a user
+    # Check that token is present
     return failure(error: :security_token_not_present) if params[:token] == nil
-    user = User.find_by(oauth_token: params[:token])
+    # check that the token returns a user
+    # TODO: do we want to check that the token returns a user and that the user = @current_user
+    user = User.find_by(oauth_token: params.fetch(:token))
     # oauth token does not exist for a user
-    return failure(error: :token_does_not_belong_to_a_user_i_apologise_immenseley_for_this_error_and_hope_that_it_will_not_arrise_again_please_call_the_toll_free_number_in_order_to_make_a_fix) if user == nil
+    return failure(error: :no_token_for_user) if user == nil
     # Token is expired
     return failure(error: :token_is_expired) if user[:oauth_expires_at].to_time < Time.now
     return success(data: user)

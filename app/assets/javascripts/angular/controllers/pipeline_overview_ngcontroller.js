1cf97f3f9c2921433368ce1517a66bb577f85f84
andrewpei <andrewpei@gmail.com>
2014-08-18 11:10:26 -0700

    Added pipeline data to overview page

--- app/assets/javascripts/angular/controllers/pipeline_overview_ngcontroller.js
+++ app/assets/javascripts/angular/controllers/pipeline_overview_ngcontroller.js
@@ -1,28 +1,29 @@
 app.controller('PipelineOverviewCtrl', ['$scope', '$resource',
   '$routeParams','PipelinesRsc', 'StagesRsc', 'ContactsBoxRsc',
   function($scope, $routeParams, $resource, PipelinesRsc, StagesRsc, ContactsBoxRsc) {
-    $scope.pipeline_id = $routeParams.id;
-    // $scope.pipelines = PipelinesRsc.query()
+    PipelinesRsc.query().$promise.then(function(pipelines_data) {
+      console.log(pipelines_data);
+    })
 
     
-    $scope.stages = StagesRsc.query({pipeline_id: $routeParams.id});
-    $scope.contacts = ContactsBoxRsc.query({pipeline_id: $routeParams.id});
-    $scope.stageContacts = {}
+    // $scope.stages = StagesRsc.query({pipeline_id: $routeParams.id});
+    // $scope.contacts = ContactsBoxRsc.query({pipeline_id: $routeParams.id});
+    // $scope.stageContacts = {}
 
-    $scope.stages.$promise.then(function(data) {
-      for (var stageIndex = 0; stageIndex < data.length; stageIndex++) {
-        var stage = data[stageIndex];
-        $scope.stageContacts[stage.id] = [];
-      };
-      return $scope.contacts.$promise;
-    })
-    .then(function(data) {
-      var contacts = data;
-      for (var contactIndex = 0; contactIndex < contacts.length; contactIndex++) {
-        var contact = contacts[contactIndex];
-        var contactStageId = contact.stage_id;
-        $scope.stageContacts[contactStageId].push(contact);
-      };
-    });
+    // $scope.stages.$promise.then(function(data) {
+    //   for (var stageIndex = 0; stageIndex < data.length; stageIndex++) {
+    //     var stage = data[stageIndex];
+    //     $scope.stageContacts[stage.id] = [];
+    //   };
+    //   return $scope.contacts.$promise;
+    // })
+    // .then(function(data) {
+    //   var contacts = data;
+    //   for (var contactIndex = 0; contactIndex < contacts.length; contactIndex++) {
+    //     var contact = contacts[contactIndex];
+    //     var contactStageId = contact.stage_id;
+    //     $scope.stageContacts[contactStageId].push(contact);
+    //   };
+    // });
 
   }]);

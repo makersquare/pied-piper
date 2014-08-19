7d883cc8dd49dfbd9c3a38f8f7883dda57103bb3
andrewpei <andrewpei@gmail.com>
2014-08-18 18:09:42 -0700

    corrected naming bug

--- app/assets/javascripts/angular/services/pipeline_collabs_rsc.js
+++ app/assets/javascripts/angular/services/pipeline_collabs_rsc.js
@@ -1,7 +1,7 @@
 app.factory("PipelineCollabRsc", ['$resource', function ($resource) {
   return $resource(
       "/pipelines/:id/users/:user_id.json",
-      {pipeline_id: "@id", user_id: "@user_id"},
+      {id: "@id", user_id: "@user_id"},
       {
           'update':{
             method: 'PUT'

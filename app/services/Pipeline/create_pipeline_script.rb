27448b074944fd1428fcf59bf9a857a320a230bb
andrewpei <andrewpei@gmail.com>
2014-08-21 17:22:01 -0700

    fixes #119

--- app/services/Pipeline/create_pipeline_script.rb
+++ app/services/Pipeline/create_pipeline_script.rb
@@ -10,6 +10,7 @@ class CreatePipelineScript < TransactionScript
     begin
       pipeline = Pipeline.create!(name: name)
       AddUserPipeline.run(user_id: creator_id, id: pipeline.id)
+      pipeline.stages.create(name: "Default", description: "", pipeline_location: 1)
       return success(:data => pipeline)
     rescue
       return failure(:name_taken)
